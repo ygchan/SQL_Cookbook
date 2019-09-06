4076b6230427d82ec764e33a9606227d5dd1d5f6
ygchan <ygchan89@gmail.com>
2019-09-05 21:28:38 -0400

    Learned: order by, asc, desc, expression(substr)

--- Ch_02_Sorting_Query.sql
+++ Ch_02_Sorting_Query.sql
@@ -34,8 +34,45 @@ order by 3 desc;
 3 rows in set (0.00 sec)
 */
 
+-- 02. Sort by multiple fields
+-- Problem: Sort by department number ascending and then salary descending
 
+select empno, deptno, ename, job
+from emp
+order by deptno, sal desc;
 
+-- Discussion: You are generally allowed to sort by columns not selected.
+-- But you must reference the column by (name), and you are not follow 
+-- to group by items not selected.
+
+-- 03. Sorting by substrings
+-- Problem: Sort by the last 2 characters in the job field
+
+select ename, job, substr(job, length(job)-1)
+from emp
+order by substr(job, length(job)-1);
+
+/* Output:
++--------+-----------+----------------------------+
+| ename  | job       | substr(job, length(job)-1) |
++--------+-----------+----------------------------+
+| TURNER | SALESMAN  | AN                         |
+| ALLEN  | SALESMAN  | AN                         |
+| WARD   | SALESMAN  | AN                         |
+| MARTIN | SALESMAN  | AN                         |
+| CLARK  | MANAGER   | ER                         |
+| BLAKE  | MANAGER   | ER                         |
+| JONES  | MANAGER   | ER                         |
+| KING   | PRESIDENT | NT                         |
+| JAMES  | CLERK     | RK                         |
+| SMITH  | CLERK     | RK                         |
+| ADAMS  | CLERK     | RK                         |
+| MILLER | CLERK     | RK                         |
+| FORD   | ANALYST   | ST                         |
+| SCOTT  | ANALYST   | ST                         |
++--------+-----------+----------------------------+
+14 rows in set (0.01 sec)
+*/
 
 
 
