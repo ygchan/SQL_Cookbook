8414e4245a55bff72b2bc93488c617d3b6a82839
ygchan <ygchan89@gmail.com>
2019-09-06 22:15:24 -0400

    Learned: stacking data atop another

--- Ch_03_Multiple_Table.sql
+++ Ch_03_Multiple_Table.sql
@@ -4,4 +4,27 @@
 -- Chapter 03: Working with multiple tables
 
 -- 01: Stacking one data rowset atop another
+select ename as ename_and_dname, deptno
+from emp
+where deptno = 10
+union all
+select '--------', '--------'
+union all
+select dname, deptno
+from dept;
 
+/* Output:
++-----------------+----------+
+| ename_and_dname | deptno   |
++-----------------+----------+
+| CLARK           | 10       |
+| KING            | 10       |
+| MILLER          | 10       |
+| --------        | -------- |
+| ACCOUNTING      | 10       |
+| RESEARCH        | 20       |
+| SALES           | 30       |
+| OPERATIONS      | 40       |
++-----------------+----------+
+8 rows in set (0.00 sec)
+*/
