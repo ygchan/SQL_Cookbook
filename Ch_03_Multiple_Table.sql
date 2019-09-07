8215eab3c18f4a00cdf9d60ade5f1f8a6cea45c8
ygchan <ygchan89@gmail.com>
2019-09-07 06:46:51 -0400

    Learned: union vs union all

--- Ch_03_Multiple_Table.sql
+++ Ch_03_Multiple_Table.sql
@@ -28,3 +28,54 @@ from dept;
 +-----------------+----------+
 8 rows in set (0.00 sec)
 */
+
+-- Important Note: Union all will include duplicates if they exits
+-- Union will filter out any duplicates.
+
+-- Union is the same as select distinct.
+-- If you know in advance there are no duplicates, then please use union all
+-- for faster performance.
+
+-- Source: c-sharpcorner.com
+-- https://bit.ly/2k4Owzj
+
+select deptno
+from emp
+union
+select deptno
+from emp;
+
+-- This will remove any duplicates, I wonder how is SQL implemented in the back
+-- Specifying union will most likely result in a sort operation in order to
+-- eliminate duplicates. The query about is roughly equivalent to the following
+
+select distinct 
+from (
+	select deptno
+	from emp
+	union all
+	select deptno
+	from emp
+);
+
+-- 02. Combining related rows
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
