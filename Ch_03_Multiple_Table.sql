6c39e0f9c9ba2137d1d7d150c7c42bd54996e07f
ygchan <ygchan89@gmail.com>
2019-09-06 22:12:08 -0400

    Refresh

--- Ch_03_Multiple_Table.sql
+++ Ch_03_Multiple_Table.sql
@@ -4,22 +4,9 @@
 -- Chapter 03: Working with multiple tables
 
 -- 00: TSQL
-create table person
-(
-	person_id smallint unsigned,
-	fname varchar(20),
-	lname varchar(20),
-	-- gender char(1),
-	-- enforce the check of gender
-	gender enum('M', 'F'),
-	birth_date date,
-	street varchar(30),
-	city varchar(20),
-	state varchar(20),
-	country varchar(20),
-	postal_code varchar(20),
-	constraint pk_person primary key (person_id)
-);
+FLUSH PRIVILEGES;
+[mysql dir]/bin/mysql -h hostname -u root -p;
+create database [databasename];
 
 -- 01: Stacking one data rowset atop another
 
