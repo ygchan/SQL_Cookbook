ff6afe9efeda28bdcfafd92e68641c079ff67f63
ygchan <ygchan89@gmail.com>
2019-09-27 20:20:34 -0400

    Learned: information_schema.tables

--- Ch_05_Meta_Data.sql
+++ Ch_05_Meta_Data.sql
@@ -1,3 +1,50 @@
 -- SQL Cookbook
 -- http://shop.oreilly.com/product/9780596009762.do
 
+-- Meta data, find information about a given schema.
+-- Such as what tables are there and which foreignt keys are not indexed
+-- You may answer all of these question by writing query against metadata.
+
+-- The book assume there is a schema called SMEAGOL.
+
+-- 01. Listing table in a schema
+-- Problem: You want to see a list of all tables you have created.
+
+select table_name
+from information_schema.tables
+where table_schema = 'bank';
+
+/* Output:
++------------------------+
+| table_name             |
++------------------------+
+| DEPT                   |
+| EMP                    |
+| account                |
+| branch                 |
+| branch_avil_balance_vw |
+| business               |
+| business_customer_vw   |
+| claims                 |
+| customer               |
+| customer_totals        |
+| customer_totals_vw     |
+| customer_vw            |
+| department             |
+| dept_2                 |
+| emloyee_vw             |
+| employee               |
+| employee_org_chart_vw  |
+| employee_vw            |
+| individual             |
+| number_tbl             |
+| officer                |
+| product                |
+| product_type           |
+| some_transcation       |
+| string_tbl             |
+| transaction            |
+| vw_customer            |
++------------------------+
+29 rows in set (0.01 sec)
+*/
