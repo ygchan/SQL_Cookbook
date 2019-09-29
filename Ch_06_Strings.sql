8031dbc6182c65e23495cc0d122a49c5eab3ea2e
ygchan <ygchan89@gmail.com>
2019-09-29 15:30:34 -0400

    Introduction

--- Ch_06_Strings.sql
+++ Ch_06_Strings.sql
@@ -2,3 +2,32 @@
 -- http://shop.oreilly.com/product/9780596009762.do
 
 -- Chapter 06: Working with strings
+-- Chapter 06 focuses on string manipulation in SQL. Interestingly author 
+-- already stated on the second line, that SQL is not designed to perform
+-- complex string manipulation. It makes me wonder if company XYZ is using
+-- Java and Python for data processing? (Extract and Transformation).
+
+-- The first and most important fundamental operation: traverse a string
+-- one character at a time. (SQL does not have a loop).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
