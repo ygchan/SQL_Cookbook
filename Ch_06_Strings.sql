6f0cad0878128d6c0e7a28ba99289c66d2387fca
ygchan <ygchan89@gmail.com>
2019-09-29 21:51:26 -0400

    Learned: step by step traverse string

--- Ch_06_Strings.sql
+++ Ch_06_Strings.sql
@@ -47,10 +47,56 @@ where iter.pos <= length(e.ename);
 -- Discussion: Use a cartesian product to generate the number of rows need
 -- to return each character of string on its own line.
 
+-- George: First you need to create a t(traverse) table using simple create
+-- table statement and insert statement. Then you will use the old ascii join
+-- format. Where condition ensure you don't have additional "empty/blank" rows.
+-- See there is iter.pos <= length(e.ename). Then the substring method selected
+-- that character corresponding to the pos.
 
+-- This is what it looks like without the filer and substr method
+select ename, iter.pos
+from (select ename from emp where ename = 'KING') e,
+     (select id as pos from t10) iter;
 
+/* Output: 
++-------+------+
+| ename | pos  |
++-------+------+
+| KING  |    1 |
+| KING  |    2 |
+| KING  |    3 |
+| KING  |    4 |
+| KING  |    5 |
+| KING  |    6 |
+| KING  |    7 |
+| KING  |    8 |
+| KING  |    9 |
+| KING  |   10 |
++-------+------+
+10 rows in set (0.07 sec)
+*/
 
+-- The cartesian product is 10 rows, this is the first step.
+-- It is common practice to refer table t10 as pivot table.
 
+-- Step two: limit the outcome to only the length of the word
+select ename, iter.pos
+from (select ename from emp where ename = 'KING') e,
+     (select id as pos from t10) iter
+where iter.pos <= length(e.ename); 
+/* Instead of hardcode, it find out the length here (4) */
+
+/* Output: 
++-------+------+
+| ename | pos  |
++-------+------+
+| KING  |    1 |
+| KING  |    2 |
+| KING  |    3 |
+| KING  |    4 |
++-------+------+
+4 rows in set (0.02 sec)
+*/
 
 
 
