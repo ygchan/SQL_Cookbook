f1c4becbf689de618b0d789c8a41b6d5d995a468
George Chan <ygchan89@gmail.com>
2019-10-09 22:33:12 -0400

    Add a file

---
+++ Ch_07_Number.sql
@@ -0,0 +1,5 @@
+-- SQL Cookbook
+-- http://shop.oreilly.com/product/9780596009762.do
+
+-- Common operations with numbers, such as numeric computations.
+-- Think about use of aggregate functions and the group by clause.
