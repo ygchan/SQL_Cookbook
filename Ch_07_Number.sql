359dd55ee9ba8fb11b0f3f42b26f66382f7dc472
ygchan <ygchan89@gmail.com>
2019-10-10 22:13:48 -0400

    Setup: Compute an average

--- Ch_07_Number.sql
+++ Ch_07_Number.sql
@@ -3,3 +3,8 @@
 
 -- Common operations with numbers, such as numeric computations.
 -- Think about use of aggregate functions and the group by clause.
+
+-- 01. Computing an Average
+-- Please compute the average salary for all employees
+-- and average salary for each department.
+
