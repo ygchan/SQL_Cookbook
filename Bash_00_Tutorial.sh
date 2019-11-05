d1b11981b27307cf6a8922ce1bafe4fd82c6cc43
ygchan <ygchan89@gmail.com>
2019-11-05 07:37:35 -0500

    Learned: quote quotes preserve spaces

--- Bash_00_Tutorial.sh
+++ Bash_00_Tutorial.sh
@@ -5,3 +5,16 @@ echo 'Hello World'
 name=George
 # Substitute the variable name with your own
 echo "My name is $name"
+
+# What about if you want to have a command in the variable
+command_string=`ls`
+# echo "Files" $command_string
+
+# Backslash to escape special character
+price_per_apple=5
+echo "The price of an Apple is: \$HK $price_per_apple"
+
+# Put a variable has spaces inside double quotes
+# to preserve the spaces
+greeting='Hello        world!'
+echo $greeting" now with spaces : $greeting"
