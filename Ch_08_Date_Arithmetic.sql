773bc495d87f33494d3b16809c8160d625b9a9a6
ygchan <ygchan89@gmail.com>
2019-11-05 07:13:11 -0500

    Add a file

---
+++ Ch_08_Date_Arithmetic.sql
@@ -0,0 +1,10 @@
+-- SQL Cookbook
+-- http://shop.oreilly.com/product/9780596009762.do
+
+-- Configurate MySQL Database
+-- export PATH=$PATH:/usr/local/mysql/bin
+-- mysql -u Study -p bank
+
+-- Study of common tasks like adding days to date, finding the number of 
+-- business day (or how many friday), between dates and finding the 
+-- difference between dates in days.
