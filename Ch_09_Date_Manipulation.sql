8dfdf82a382231c352d74f2c454d231696378091
ygchan <ygchan89@gmail.com>
2019-11-13 20:57:06 -0500

    Learned: day, last_day, date_add function

---
+++ Ch_09_Date_Manipulation.sql
@@ -0,0 +1,86 @@
+-- SQL Cookbook
+-- http://shop.oreilly.com/product/9780596009762.do
+
+-- Configurate MySQL Database
+-- export PATH=$PATH:/usr/local/mysql/bin
+-- mysql -u Study -p bank
+
+-- CHAPTER 9: Date Manipulation
+-- Let's learn togther how to search and modifying dates.
+-- Query involving dates - first of the month, last of the month, last friday
+-- etc are very common in order to build reports.
+
+-- 01. Determining if a year is a leap year
+-- Please confirm if the current year is a leap year.
+
+/* Step #01. Getting the 12/31 of the previous year */
+select date_add(current_date, interval -dayofyear(current_date) day)
+from t1;
+
+/* Output:
++---------------------------------------------------------------+
+| date_add(current_date, interval -dayofyear(current_date) day) |
++---------------------------------------------------------------+
+| 2018-12-31                                                    |
++---------------------------------------------------------------+
+1 row in set (0.00 sec)
+*/
+
+/* Step #02. Add 1 day to it */
+select 
+   date_add(
+      date_add(current_date, interval -dayofyear(current_date) day),
+      interval 1 day
+   ) dy -- Shorten the name
+from t1;
+
+/* Step #03. Add 1 month to it */
+/* Step #04. Get the last day (using last_day) method */
+/* Step #05. Get the day of the current day generated */
+select year(current_date) year,
+   day(
+   last_day(
+   date_add(
+   date_add(
+   date_add(current_date, 
+      interval -dayofyear(current_date) day),
+      interval 1 day),
+      interval 1 month))) dy
+from t1;
+
+/* Output: 
++------+------+
+| year | dy   |
++------+------+
+| 2019 |   28 |
++------+------+
+1 row in set (0.00 sec)
+*/
+
+-- George: We found out that 2019 is not a leap year.
+
+-- In the last example, the author pointed out we should be using x.*
+select x.*, case when dy = 28 then 'NOT LEAP YEAR'
+            when dy = 29 then 'LEAP YEAR'
+            else '' end as Summary
+from (
+select year(current_date) year,
+   day(
+   last_day(
+   date_add(
+   date_add(
+   date_add(current_date, 
+      interval -dayofyear(current_date) day),
+      interval 1 day),
+      interval 1 month))) dy
+from t1
+) x;
+
+/* Output:
++------+------+---------------+
+| year | dy   | Summary       |
++------+------+---------------+
+| 2019 |   28 | NOT LEAP YEAR |
++------+------+---------------+
+1 row in set (0.00 sec)
+*/
