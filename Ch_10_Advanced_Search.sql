ebc6f73574b5df7ca5b3b5d8437c1cf8fbeae0a2
ygchan <ygchan89@gmail.com>
2019-11-15 21:04:54 -0500

    Add a file

---
+++ Ch_10_Advanced_Search.sql
@@ -0,0 +1,12 @@
+-- SQL Cookbook
+-- http://shop.oreilly.com/product/9780596009762.do
+
+-- Configurate MySQL Database
+-- export PATH=$PATH:/usr/local/mysql/bin
+-- mysql -u Study -p bank
+
+-- CHAPTER 11: Advanced Searching
+-- The most basic kind of query are the types with where clauses and
+-- grouping techniques to search out and return the results you need.
+
+
