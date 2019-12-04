1c900cfbb5a05879064995f9bdde79451b1207df
George Chan <ygchan89@gmail.com>
2019-12-03 21:26:52 -0500

    Update solution to 10.4 filling in missing values

--- Ch_10_Advanced_Search.sql
+++ Ch_10_Advanced_Search.sql
@@ -182,3 +182,18 @@ order by 2;
 
 -- Happy Thanks Giving! :)
 -- Back from Vacation ! :)
+
+select y.yr, coalesce(x.cnt, 0) as cnt
+from (
+select min_year-mod(cast(min_year as int), 10)+rn as yr
+from (
+select (select min(extract(year from hiredate))
+    from emp) as min_year,
+    id-1 as rn
+from t10) a
+) y
+left join (select extract(year from hiredate) as yr, count(*) as cnt
+from emp
+group by extract(year from hiredate)
+) x
+on (y.yr = x.yr);
