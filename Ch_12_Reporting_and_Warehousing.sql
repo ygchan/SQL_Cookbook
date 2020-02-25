3629ec756474ac5480ad200714b0df002e7d104e
ygchan <ygchan89@gmail.com>
2020-02-24 22:52:02 -0500

    Add a file

---
+++ Ch_12_Reporting_and_Warehousing.sql
@@ -0,0 +1,13 @@
+-- SQL Cookbook
+-- http://shop.oreilly.com/product/9780596009762.do
+
+-- Configurate MySQL Database
+-- export PATH=$PATH:/usr/local/mysql/bin
+-- mysql -u Study -p bank
+
+-- CHAPTER 12. Reporting and Warehousing
+-- The queries that will be helpful for creating reports. They usually
+-- involves reporting specific formatting with different level of aggregation.
+-- Another focus is on transposing / pivoting result sets, converting rows
+-- into columns. Pivoting is extremely important topic.
+
