4acb2ebd28616bc927a07c1628a78883003ab6b5
ygchan <ygchan89@gmail.com>
2020-02-25 22:16:48 -0500

    Learned: pivot results into multiple rows'

--- Ch_12_Reporting_and_Warehousing.sql
+++ Ch_12_Reporting_and_Warehousing.sql
@@ -77,11 +77,73 @@ from emp p
 group by deptno
 ) x;
 
+-- 02. Pivoting Result set into multiple rows
+-- Turning rows into columns, with one row having one value.
 
+-- step 1: grouping each job and employee name
+select e.job,
+	e.ename,
+	(select count(*) from emp d
+	where e.job = d.job and e.empno < d.empno) as rank
+from emp e;
 
+/* Output:
++-----------+--------+------+
+| job       | ename  | rank |
++-----------+--------+------+
+| CLERK     | SMITH  |    3 |
+| SALESMAN  | ALLEN  |    3 |
+| SALESMAN  | WARD   |    2 |
+| MANAGER   | JONES  |    2 |
+| SALESMAN  | MARTIN |    1 |
+| MANAGER   | BLAKE  |    1 |
+| MANAGER   | CLARK  |    0 |
+| ANALYST   | SCOTT  |    1 |
+| PRESIDENT | KING   |    0 |
+| SALESMAN  | TURNER |    0 |
+| CLERK     | ADAMS  |    2 |
+| CLERK     | JAMES  |    1 |
+| ANALYST   | FORD   |    0 |
+| CLERK     | MILLER |    0 |
++-----------+--------+------+
+*/
 
+-- step 2: pivot the table using result set (case expression)
+-- and the aggregate function max while grouping the values.
 
+select 
+	max(case when job = 'CLERK' then ename end) as CLERK,
+  max(case when job = 'SALESMAN' then ename end) as SALESMAN,
+  max(case when job = 'MANAGER' then ename end) as MANAGER,
+  max(case when job = 'ANALYST' then ename end) as ANALYST
+from (
+select e.job,
+	e.ename,
+	(select count(*) from emp d
+	where e.job = d.job and e.empno < d.empno) as rank
+from emp e
+) x
+group by rank;
 
+/* Output:
++--------+----------+---------+---------+
+| CLERK  | SALESMAN | MANAGER | ANALYST |
++--------+----------+---------+---------+
+| MILLER | TURNER   | CLARK   | FORD    |
+| JAMES  | MARTIN   | BLAKE   | SCOTT   |
+| ADAMS  | WARD     | JONES   | NULL    |
+| SMITH  | ALLEN    | NULL    | NULL    |
++--------+----------+---------+---------+
+4 rows in set (0.00 sec)
+*/
+
+-- Using the previous solution does not work, because you want the data
+-- returned in multiple rows. First you need to group by the job and employee
+-- name, assign a rank so you can group the scalar subquery.
+
+-- Repeat: Use a scalar subquery to rank each employee by Empno, pivot the
+-- result set using case expression and the aggregate function max while 
+-- grouping on the value returned by the scalary subquery.
 
 
 
