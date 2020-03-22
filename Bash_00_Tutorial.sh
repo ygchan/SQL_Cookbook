7a61c8461d56f8f935da5064928e5d4477936929
George Chan <ygchan89@gmail.com>
2020-03-21 23:40:57 -0400

    Create a backup

--- Bash_00_Tutorial.sh
+++ Bash_00_Tutorial.sh
@@ -56,3 +56,13 @@ echo "A $Lunch costs just $6"
 
 # Moving a file
 mv /path/to/source /path/to/destination
+
+#!/bin/bash
+# This bash script is used to backup a user's home directory to /tmp/.
+user=$(whoami)
+input=/home/$user
+output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
+
+tar -czf $output $input
+echo "Backup of $input completed! Details about the output backup file:"
+ls -l $output
